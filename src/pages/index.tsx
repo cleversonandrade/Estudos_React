dbbf55428739e907fe669863dffae139e19416e3
Cleverson Andrade <cleversonandrade62@gmail.com>
2024-06-07 22:48:39 -0300

    aula05_funcoes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,22 @@
-//import Image from "next/image";
-//import { Inter } from "next/font/google";
-//const inter = Inter({ subsets: ["latin"] });
 
+const nome = 'Cleverson';
+const curso = 'React com Typescript';
+const instituicao = 'CFB - Cursos.';
+
+const Topo = () => {
+  return(
+    <div className="flex-col border-none h-32 bg-blue-300 ">
+      <div>{nome}</div>
+      <div>{curso}</div>
+      <div>{instituicao}</div>
+    </div>
+  );
+}
 
 export default function Home() {
-  const nome = 'Cleverson';
-  const curso = 'React com Typescript';
-  const instituicao = 'CFB - Cursos.';
-
-  function retnome(){
-    return nome
-  }
-
   return (
-    <div className="flex-col border-none h-32 bg-blue-300 ">
-      <div>{`Nome do aluno: ${nome}`}</div>
-      <div>{instituicao}</div>
-      <div>{retnome()}</div>
-      <div>{`Estudando atualmente: ${curso}`}</div>
+    <div>
+      <Topo />
     </div>
   );
 }
