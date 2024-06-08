9b37efca856a68b4fbbef7c974da17bd451a01be
Cleverson Andrade <cleversonandrade62@gmail.com>
2024-06-07 21:58:22 -0300

    aula_03

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,22 @@
 //import { Inter } from "next/font/google";
 //const inter = Inter({ subsets: ["latin"] });
 
+
 export default function Home() {
+  const nome = 'Cleverson';
+  const curso = 'React com Typescript';
+  const instituicao = 'CFB - Cursos.';
+
+  function retnome(){
+    return nome
+  }
+
   return (
-    <div>
-      <div>CFB - Cursos</div>
-      <div>Curso de React Next</div>
-      <div>Typescript</div>
-      <div>React</div>
+    <div className="flex-col border-none h-32 bg-blue-300 ">
+      <div>{`Nome do aluno: ${nome}`}</div>
+      <div>{instituicao}</div>
+      <div>{retnome()}</div>
+      <div>{`Estudando atualmente: ${curso}`}</div>
     </div>
   );
 }
