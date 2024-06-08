8b9d8043d1c126a2e6faa842575a8328702a7d57
Cleverson Andrade <cleversonandrade62@gmail.com>
2024-06-08 11:11:09 -0300

    c_r_a006

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,6 +11,7 @@ const Topo = () => {
       <div className="flex flex-col justify-center items-center"> 
         <div className="text-3xl">{nome}</div>
         <div>{curso}</div>
+        <div style={{ color: '#f00', backgroundColor: '#bbb' }}>React</div>
       </div>
       <div>{instituicao}</div>
     </div>
@@ -21,6 +22,19 @@ export default function Home() {
   return (
     <div>
       <Topo />
+      <div style={testecss}>
+        <div>Curso de React</div>
+        <div>TypeScrip</div>
+      </div>
     </div>
   );
+}
+
+const testecss = {
+  display: 'flex',
+  justifyContent: 'center',
+  alignItens: 'center',
+  color: '#00f',
+  backgroundColor: '#eee',
+  fontSize: '20px'
 }
