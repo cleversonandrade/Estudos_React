1c03ced0d927a2a002d92a25c609e07f2f009bb3
Cleverson Andrade <cleversonandrade62@gmail.com>
2024-06-13 20:07:50 -0300

    listas_react

--- src/pages/inputs/inputs.tsx
+++ src/pages/inputs/inputs.tsx
@@ -1,10 +1,43 @@
+/* eslint-disable react/jsx-key */
 import Topo from "@/components/Topo";
 import { useState } from "react"
 
-export default function Inputs() {
 
+const cursos = ['','HTML','CSS','JavaScript','React','Typescript','Node',]
+const ncursos = [
+    {
+        'curso':'HTML'
+    },
+    {
+        'curso':'CSS'
+    },
+    {
+        'curso':'JavaScript'
+    },
+    {
+        'curso':'React'
+    },
+    {
+        'curso':'Typescript'
+    },
+    {
+        'curso':'Node'
+    }
+]
+
+export default function Inputs() {
     const [nome, setNome] = useState<string>('');
-    const [curso, setCurso] = useState<string>('');
+    const [curso, setCurso] = useState<string>(cursos[0]);
+
+    function fCursos(){
+        return cursos.map((e:any) => {
+            return <option value={e}>{e}</option>
+        })
+    }
+
+   const ccurso = ncursos.map((e:any) => {
+    return <option value={e.curso}>{e.curso}</option>
+   })
 
     return(
         <div>
@@ -16,13 +49,9 @@ export default function Inputs() {
         <div className="campoForm">
         <label>Cursos</label>
         <select value={curso} onChange={(e) => setCurso(e.target.value)}>
-            <option value=""></option>
-            <option value="HTML">HTML</option>
-            <option value="CSS">CSS</option>
-            <option value="JavaScript">JavaScript</option>
-            <option value="React">React</option>
-            <option value="Typescript">Typescript</option>
-            <option value="Node">Node</option>
+            {
+               ccurso
+            }
         </select> 
         </div>
         <div>Nome digitado: {nome}</div>
